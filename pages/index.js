0bacf790f1f8fd6800eebdd2cc163636a58de7ee
Anish Soni <anish.21cse189@rtu.ac.in>
2024-03-30 21:59:25 +0530

    updated the projects href

--- pages/index.js
+++ pages/index.js
@@ -82,12 +82,12 @@ export default function Home() {
         </section>
         <section>
           <div>
-            <a href="https://github.com/anishsoni29">
+            <a href="https://github.com/anishsoni29?tab=repositories">
               <button
                 type="button"
                 class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 mb-10"
               >
-                Services
+                Projects
               </button>
             </a>
 
