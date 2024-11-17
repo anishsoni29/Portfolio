838be2572e400e73165768d0a993de0032baebe7
gauravpurohit28 <sonianish441@gmail.com>
2024-11-17 19:45:46 +0530

    updated with the proper dimentions of the image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,10 +31,14 @@ export default function Page() {
               />
             </div>
             <BlurFade delay={BLUR_FADE_DELAY}>
-              <Avatar className="size-28 border">
-                <AvatarImage alt={DATA.name} src={DATA.avatarUrl} />
-                <AvatarFallback>{DATA.initials}</AvatarFallback>
-              </Avatar>
+            <Avatar className="w-28 h-28 border">
+              <AvatarImage
+                alt={DATA.name}
+                src={DATA.avatarUrl}
+                className="rounded-full object-cover w-full h-full"
+              />
+              <AvatarFallback>{DATA.initials}</AvatarFallback>
+            </Avatar>
             </BlurFade>
           </div>
         </div>
